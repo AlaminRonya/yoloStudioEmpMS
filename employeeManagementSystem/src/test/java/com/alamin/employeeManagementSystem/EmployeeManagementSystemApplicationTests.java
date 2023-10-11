79495ff917fe320c00c44a5f1605ddded602bbfc
AlaminRonya <hprony12@gmail.com>
2023-10-11 15:23:06 +0600

    Project Initial

---
+++ employeeManagementSystem/src/test/java/com/alamin/employeeManagementSystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.employeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
